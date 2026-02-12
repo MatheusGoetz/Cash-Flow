ac5f7f64aae42f3a849c0b716943d0b648387065
Matheus Goetz <goetz.dev@gmail.com>
2026-02-12 19:59:16 -0300

    Feat: Criado endpoint de registrar despesa e UseCase para regra de negocio de registrar despesa

--- CashFlow.Infrastructure/obj/Debug/net8.0/CashFlow.Infrastructure.AssemblyInfo.cs
+++ CashFlow.Infrastructure/obj/Debug/net8.0/CashFlow.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fec03df8dcb4c1bfee8b1e2ecb09455a863f007")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
